0b16d2b13d1d9f5737c82b681ad3c5783602c863
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-05 13:58:09 -0300

    chore: add imports

--- CqrsDemo.Infrastructure/Context/AppDbContext.cs
+++ CqrsDemo.Infrastructure/Context/AppDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CqrsDemo.Domain.Entities;
 using CqrsDemo.Infrastructure.EntityConfiguration;
 using Microsoft.EntityFrameworkCore;
 
--- CqrsDemo.Infrastructure/EntityConfiguration/MemberConfiguration.cs
+++ CqrsDemo.Infrastructure/EntityConfiguration/MemberConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CqrsDemo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
