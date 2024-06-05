db9010e68b1074ba4015665effef551408e42784
vitosandrin <sandrinvito10@gmail.com>
2024-06-04 23:52:39 -0300

    feat: create DomainValidation

---
+++ CqrsDemo.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CqrsDemo.Domain.Validation;
+
+public class DomainValidation : Exception
+{
+    public DomainValidation(string error) : base(error)
+    {
+    }
+
+    public static void When(bool hasError, string error)
+    {
+        if (hasError)
+        {
+            throw new DomainValidation(error);
+        }
+    }
+}
