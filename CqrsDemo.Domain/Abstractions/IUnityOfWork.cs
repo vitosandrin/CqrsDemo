2feeb66fda45164c456755f0eff61de47025fe0c
vitosandrin <sandrinvito10@gmail.com>
2024-06-05 00:06:39 -0300

    feat: create IUnityOfWork interface

---
+++ CqrsDemo.Domain/Abstractions/IUnityOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CqrsDemo.Domain.Abstractions;
+
+public interface IUnityOfWork
+{
+    IMemberRepository MemberRepository { get; }
+    Task CommitAsync();
+    Task Rollback();
+}
