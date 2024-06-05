637889d2bd67011a31f79f35e4b44aab816170ba
vitosandrin <sandrinvito10@gmail.com>
2024-06-05 00:18:11 -0300

    feat: add MemberCOnfig for migrations

---
+++ CqrsDemo.Infrastructure/EntityConfiguration/MemberConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CqrsDemo.Infrastructure.EntityConfiguration;
+
+public class MemberConfiguration : IEntityTypeConfiguration<Member>
+{
+    public void Configure(EntityTypeBuilder<Member> builder)
+    {
+        builder.HasKey(m => m.Id);
+        builder.Property(m => m.FirstName).HasMaxLength(100).IsRequired();
+        builder.Property(m => m.LastName).HasMaxLength(100).IsRequired();
+        builder.Property(m => m.Email).HasMaxLength(150).IsRequired();
+        builder.Property(m => m.IsActive).IsRequired();
+
+        builder.HasData(
+            new Member(1, "Laney", "Staley", "laney@email.com", true),
+            new Member(2, "Kurt", "Cobain", "kurt@email.com", true)
+        );
+    }
+}
