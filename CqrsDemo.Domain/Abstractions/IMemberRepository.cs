ce36e50a7acfe57d5efc559ab932ae99eac419d9
vitosandrin <sandrinvito10@gmail.com>
2024-06-04 23:52:18 -0300

    feat: create domain member repository

---
+++ CqrsDemo.Domain/Abstractions/IMemberRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CqrsDemo.Domain.Abstractions;
+
+public interface IMemberRepository
+{
+    Task<IEnumerable<Member>> GetMembers();
+    Task<Member> GetMemberById(int memberId);
+    Task<Member> AddMember(Member member);
+    void UpdateMember(Member member);
+    Task<Member> DeleteMember(int memberId);
+}
