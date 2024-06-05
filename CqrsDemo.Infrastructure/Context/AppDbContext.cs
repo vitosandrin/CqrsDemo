d514d103f4058b74d5d682dec2b47486baa2477f
vitosandrin <sandrinvito10@gmail.com>
2024-06-05 00:18:00 -0300

    wip: DbContext

---
+++ CqrsDemo.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CqrsDemo.Infrastructure.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Member> Members { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new MemberConfiguration());
+    }
+}
