e6888898c66e2114ba97914697dc36de61b3dbd4
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-06 10:08:48 -0300

    ok

--- CqrsDemo.Application/Members/Commands/MemberCommandBase.cs
+++ CqrsDemo.Application/Members/Commands/MemberCommandBase.cs
@@ -7,7 +7,6 @@ public abstract class MemberCommandBase : IRequest<Member>
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? Gender { get; set; }
     public string? Email { get; set; }
     public bool? IsActive { get; set; }
 }
