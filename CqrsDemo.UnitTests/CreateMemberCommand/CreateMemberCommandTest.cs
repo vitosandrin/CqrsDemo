bf561de6c052e41ed292f2ca2b5a9bca1ab8fff2
vitosandrin <sandrinvito10@gmail.com>
2024-06-08 23:14:43 -0300

    Updated unit tests and added package references
    Updated the `CqrsDemo.UnitTests.csproj` file to include new package references. Modified the `CreateMemberCommandTest.cs` file to include new `using` directives and updated the `CreateMemberCommandTest` class to inherit from `IClassFixture<CreateMemberCommandSetup>`. Added three new test methods to the `CreateMemberCommandTest` class to test the `CreateMemberCommand` and `UpdateMemberCommand` functionality.

--- CqrsDemo.UnitTests/CreateMemberCommand/CreateMemberCommandTest.cs
+++ CqrsDemo.UnitTests/CreateMemberCommand/CreateMemberCommandTest.cs
@@ -1,11 +1,62 @@
-﻿using System;
+﻿using Azure.Core;
+using CqrsDemo.Application.Members.Commands;
+using CqrsDemo.Application.Members.Commands.Notifications;
+using CqrsDemo.Domain.Entities;
+using CqrsDemo.Domain.Validation;
+using MediatR;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CqrsDemo.UnitTests;
 
-public class CreateMemberCommandTest
+public class CreateMemberCommandTest(CreateMemberCommandSetup createMemberCommandSetup) : IClassFixture<CreateMemberCommandSetup>
 {
+    private readonly Member _member = createMemberCommandSetup._uow._member1;
+    [Fact]
+    public void CreateMemberCommand_FirstNameIsEmpty_ReturnsErrorMessage()
+    {
+        var command = new CreateMemberCommand
+        {
+            Email = _member.Email,
+            FirstName = "",
+            LastName = _member.LastName,
+            IsActive = _member.IsActive
+        };
+
+        var exception = Assert.Throws<DomainValidation>(() => new Member("", "email@email.com", "LastnName", true));
+        Assert.Equal("Invalid name. FirstName is required", exception.Message);
+    }
+
+    [Fact]
+    public void CreateMemberCommand_ReturnsSucess()
+    {
+        var member = new CreateMemberCommand
+        {
+            Email = _member.Email,
+            FirstName = _member.FirstName,
+            LastName = _member.LastName,
+            IsActive = _member.IsActive
+        };
+
+        Assert.Equal(_member.FirstName, member.FirstName);
+    }
+
+    [Fact]
+    public void UpdateMemberCommand_ReturnSucess()
+    {
+        var member = new UpdateMemberCommand
+        {
+            Email = _member.Email,
+            FirstName = _member.FirstName,
+            LastName = _member.LastName,
+            IsActive = _member.IsActive
+        };
+
+        Assert.Equal(_member.FirstName, member.FirstName);
+    }
 }
