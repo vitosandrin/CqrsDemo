d3893b984118f8aa6d94d9824e10aee1d38ce4f2
vitosandrin <sandrinvito10@gmail.com>
2024-06-08 23:14:05 -0300

    test/fix: uow setup

--- CqrsDemo.UnitTests/UnityOfWorkTests/UnityOfWorkSetup.cs
+++ CqrsDemo.UnitTests/UnityOfWorkTests/UnityOfWorkSetup.cs
@@ -9,33 +9,32 @@ using System.Text;
 using System.Threading.Tasks;
 using CqrsDemo.Infrastructure.Repositories;
 using MediatR;
+using FluentValidation;
+using CqrsDemo.Application.Members.Commands;
+using Microsoft.EntityFrameworkCore;
 
 namespace CqrsDemo.UnitTests;
 
 public class UnityOfWorkSetup
 {
     public readonly IUnityOfWork _unitOfWork;
-
-    public readonly IMediator _mediator;
-
-    public static Member _member1 = new("Member1", "Any", "member1@teste.com", true);
-    public static Member _member2 = new("Member2", "Any", "member2@teste.com", false);
-    public static List<Member> _memberList = new() { _member1, _member2 };
-    public static Member _member2Updated = new("Member2", "Any", "member2#teste.com", true);
+    public readonly Member _member1 = new("Member1", "Any", "member1@teste.com", true);
+    public readonly Member _member2 = new("Member2", "Any", "member2@teste.com", false);
+    public readonly Member _member2Updated = new("Member2", "Any", "member2#teste.com", true);
     public UnityOfWorkSetup()
     {
-        Mock<AppDbContext> contextMock = new();
-        Mock<Mediator> mediatorMock = new();
+        var options = new DbContextOptionsBuilder<AppDbContext>().Options;
+        var contextMock = new Mock<AppDbContext>(options);
         contextMock.Setup(m => m.SaveChanges()).Returns(1);
 
         Mock<IMemberRepository> memberMock = new();
-        memberMock.Setup(m => m.GetMembers()).ReturnsAsync(_memberList);
         memberMock.Setup(m => m.GetMemberById(1)).ReturnsAsync(_member1);
         memberMock.Setup(m => m.AddMember(_member2)).ReturnsAsync(_member2);
         memberMock.Setup(m => m.UpdateMember(_member2));
         memberMock.Setup(m => m.DeleteMember(1)).ReturnsAsync(_member1);
-        _unitOfWork = new UnityOfWork(contextMock.Object);
-        _mediator = mediatorMock.Object;
+
+        _unitOfWork = new UnityOfWork(contextMock.Object, memberMock.Object);
+
     }
 
 }
